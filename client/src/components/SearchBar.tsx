da8f55061e8a1ad450bcff00ae35435ba9f54f0a
Nikcet <mstislav.kr@yandex.ru>
2025-08-05 23:09:33 +0300

    add searchBar component

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,27 @@
+import { SearchBarProps } from '../types/components';
+import searchIcon from '../images/search-icon.svg';
+import './SearchBar.css';
+
+
+function SearchBar({
+    value,
+    onChange,
+    placeholder = "Поиск пользователей..."
+}: SearchBarProps) {
+    return (
+        <div className="search-container">
+            <form className="search-bar">
+                <input
+                    type="text"
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
+                    placeholder={placeholder}
+                    className="search-input"
+                />
+                <img src={searchIcon} alt="search" className="search-icon" />
+            </form>
+        </div>
+    );
+}
+
+export default SearchBar; 
