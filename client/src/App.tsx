a3855e0d9fd2132adae11dc9ba03fc34cb032189
Nikcet <mstislav.kr@yandex.ru>
2025-08-05 23:19:43 +0300

    add main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import SearchBar from './components/SearchBar';
+import UserList from './components/UserList';
+import StatusMessage from './components/StatusMessage';
+import { useUsers } from './hooks/useUsers';
 import './App.css';
 
 function App() {
+  const { users, loading, error, fetchUsers } = useUsers();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        
+        {loading && (
+          <StatusMessage 
+            type="loading" 
+            message="Загрузка пользователей..." 
+          />
+        )}
+        
+        {error && (
+          <StatusMessage 
+            type="error" 
+            message={error} 
+          />
+        )}
+        
+        {!loading && !error && (
+          <UserList users={users} />
+        )}
+      </div>
     </div>
   );
 }
